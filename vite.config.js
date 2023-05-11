56ae4325e6ec4b4dc787aa5b805c7cf1bbdf190f
flash <2509719845@qq.com>
2023-05-11 13:29:08 +0800

    覆盖element主题颜色

--- vite.config.js
+++ vite.config.js
@@ -15,12 +15,22 @@ export default defineConfig({
       resolvers: [ElementPlusResolver()],
     }),
     Components({
-      resolvers: [ElementPlusResolver()],
+      resolvers: [ElementPlusResolver({importStyle:"sass"})],
     }),
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        // 自动导入定制化样式文件进行样式覆盖
+        additionalData: `
+          @use "@/styles/element/index.scss" as *;
+        `,
+      }
     }
   }
 })
