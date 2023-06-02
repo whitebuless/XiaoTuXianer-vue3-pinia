ccfb648793c57db40f8da72f1f75c8025233b790
flash <2509719845@qq.com>
2023-06-02 09:58:32 +0800

    penel新鲜

--- src/views/Home/components/HomeNew.vue
+++ src/views/Home/components/HomeNew.vue
@@ -1,5 +1,79 @@
+<script setup>
+import HomePenel from './HomePenel.vue';
+import {findNewAPI} from '@/apis/home';
+import { onMounted, ref } from 'vue';
+
+const newList=ref([])
+
+const getNewList= async ()=>{
+  const res=await findNewAPI()
+  newList.value=res.result
+}
+
+onMounted(()=>getNewList())
+</script>
+
 <template>
-  <div>
-    我是New
-  </div>
+  <HomePenel title="新鲜好物" subTitle="新鲜出炉 品质靠谱">
+    <ul class="goods-list">
+      <li v-for="item in newList" :key="item.id">
+        <RouterLink to="/">
+          <img :src="item.picture" alt="" />
+          <p class="name">{{ item.name }}</p>
+          <p class="price">&yen;{{ item.price }}</p>
+        </RouterLink>
+      </li>
+    </ul>
+  </HomePenel>
+  <!-- 下面是插槽主体内容模版
+  <ul class="goods-list">
+    <li v-for="item in newList" :key="item.id">
+      <RouterLink to="/">
+        <img :src="item.picture" alt="" />
+        <p class="name">{{ item.name }}</p>
+        <p class="price">&yen;{{ item.price }}</p>
+      </RouterLink>
+    </li>
+  </ul>
+  -->
 </template>
+
+
+<style scoped lang='scss'>
+.goods-list {
+  display: flex;
+  justify-content: space-between;
+  height: 406px;
+
+  li {
+    width: 306px;
+    height: 406px;
+
+    background: #f0f9f4;
+    transition: all .5s;
+
+    &:hover {
+      transform: translate3d(0, -3px, 0);
+      box-shadow: 0 3px 8px rgb(0 0 0 / 20%);
+    }
+
+    img {
+      width: 306px;
+      height: 306px;
+    }
+
+    p {
+      font-size: 22px;
+      padding-top: 12px;
+      text-align: center;
+      text-overflow: ellipsis;
+      overflow: hidden;
+      white-space: nowrap;
+    }
+
+    .price {
+      color: $priceColor;
+    }
+  }
+}
+</style>
--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -4,6 +4,7 @@
   import HomeNew from './components/HomeNew.vue'
   import HomeHot from './components/HomeHot.vue'
   import homeProduct from './components/HomeProduct.vue'
+  import HomePenel from './components/HomePenel.vue'
 </script>
 
 <template>
@@ -14,4 +15,15 @@
   <HomeNew />
   <HomeHot />
   <homeProduct />
+  <!-- 测试面板组件 -->
+  <HomePenel title="新鲜好物" subTitle="新鲜好物，好多商品">
+    <div>
+      我是新鲜好物的插槽内容
+    </div>
+  </HomePenel>
+  <HomePenel title="人气推荐" subTitle="人气推荐，好多商品">
+    <div>
+      我是人气推荐 的插槽内容
+    </div>
+  </HomePenel>
 </template>
