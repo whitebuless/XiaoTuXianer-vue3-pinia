a208a61bade528e8d8e26340443185d32d9ac942
flash <2509719845@qq.com>
2023-06-02 13:07:54 +0800

    懒加载指令优化

---
+++ src/directives/index.js
@@ -0,0 +1,22 @@
+import { useIntersectionObserver } from '@vueuse/core'
+//define lazy-load plugin
+export const lazyPlugin={
+  install(app){
+    //lazy-load logic
+    app.directive('img-lazy',{
+      mounted(el,binding) {
+        console.log(el,binding.value)
+        const { stop } = useIntersectionObserver(
+          el,
+          ([{ isIntersecting }]) => {
+            if(isIntersecting){
+              //enterView
+              el.src=binding.value
+              stop
+            }
+          },
+        )
+      },
+    })
+  }
+}
--- src/main.js
+++ src/main.js
@@ -8,7 +8,9 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
-import { useIntersectionObserver } from '@vueuse/core'
+//引入懒加载
+import { lazyPlugin } from '@/directives/index'
+
 
 // // 测试接口函数
 // import { getCategory }  from '@/apis/testAPI'
@@ -21,23 +23,6 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(lazyPlugin)
 
 app.mount('#app')
-
-//定义全局指令
-app.directive('img-lazy',{
-  mounted(el,binding){
-    //el:指令绑定的元素
-    //binding：binding.value  the value after command "="
-    console.log(el,binding.value)
-    useIntersectionObserver(
-      el,
-      ([{ isIntersecting }]) => {
-        if(isIntersecting){
-          //enterView
-          el.src=binding.value
-        }
-      },
-    )
-  }
-})
