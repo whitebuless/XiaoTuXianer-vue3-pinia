84888c294e5a727d65062c1dcfb3611f10b330f1
flash <2509719845@qq.com>
2023-06-03 10:07:11 +0800

    分类基础列表渲染

--- src/views/SubCategory/index.vue
+++ src/views/SubCategory/index.vue
@@ -1,7 +1,8 @@
 <script setup>
-import {getCategoryFilterAPI} from '@/apis/category'
+import {getCategoryFilterAPI,getSubCategoryAPI} from '@/apis/category'
 import { useRoute } from 'vue-router'
 import { onMounted, ref } from 'vue'
+import GoodsItem from '../Home/components/GoodsItem.vue'
 const categoryData=ref({})
 const route = useRoute()
 const getCategoryData= async()=>{
@@ -10,6 +11,21 @@ const getCategoryData= async()=>{
 }
 
 onMounted(()=>getCategoryData())
+
+//basic data polish
+const goodList=ref([])
+const reqData=ref({
+  categoryId:route.params.parentId,
+  page:1,
+  pageSize:20,
+  sortField:'publishTime'
+})
+const getGoodList= async ()=>{
+  const res= await getSubCategoryAPI(reqData.value)
+  goodList.value=res.result.items
+}
+
+onMounted(()=>getGoodList())
 </script>
 
 <template>
@@ -31,6 +47,7 @@ onMounted(()=>getCategoryData())
       </el-tabs>
       <div class="body">
          <!-- 商品列表-->
+         <GoodsItem v-for="goods in goodList" :goods="goods" :key="goods.id"></GoodsItem>
       </div>
     </div>
   </div>
