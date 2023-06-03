4b40bc5572dbbeb15d4624ec2015610547ed662e
flash <2509719845@qq.com>
2023-06-03 11:01:41 +0800

    详情页的路由配置

--- src/views/Home/components/HomeNew.vue
+++ src/views/Home/components/HomeNew.vue
@@ -17,7 +17,7 @@ onMounted(()=>getNewList())
   <HomePenel title="新鲜好物" subTitle="新鲜出炉 品质靠谱">
     <ul class="goods-list">
       <li v-for="item in newList" :key="item.id">
-        <RouterLink to="/">
+        <RouterLink :to="`/detail/${item.id}`">
           <img :src="item.picture" alt="" />
           <p class="name">{{ item.name }}</p>
           <p class="price">&yen;{{ item.price }}</p>
