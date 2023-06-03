da52b67279f51be2a66de146baa2bede14f9259f
flash <2509719845@qq.com>
2023-06-03 12:03:53 +0800

    详情热榜基础组件封装和基础数据获取渲染

--- src/views/Detail/index.vue
+++ src/views/Detail/index.vue
@@ -2,6 +2,8 @@
 import  {getDetail} from '@/apis/detail'
 import { onMounted, ref } from 'vue'
 import { useRoute } from 'vue-router'
+import DetailHot from './components/DetailHot.vue'
+
 
 const goods=ref({})
 const route=useRoute()
@@ -115,7 +117,10 @@ onMounted(()=>getGoods())
             </div>
             <!-- 24热榜+专题推荐 -->
             <div class="goods-aside">
-
+              <!-- 24小时 -->
+              <DetailHot/>
+              <!-- 周 -->
+              <DetailHot/>
             </div>
           </div>
         </div>
