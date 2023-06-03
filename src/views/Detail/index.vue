76fc9d5ca9faa8eaf44b679294609c09fc1f47ca
flash <2509719845@qq.com>
2023-06-03 12:39:09 +0800

    图片预览组件，小兔切换大图

--- src/views/Detail/index.vue
+++ src/views/Detail/index.vue
@@ -3,6 +3,7 @@ import  {getDetail} from '@/apis/detail'
 import { onMounted, ref } from 'vue'
 import { useRoute } from 'vue-router'
 import DetailHot from './components/DetailHot.vue'
+import ImageView from '@/components/index.vue'
 
 
 const goods=ref({})
@@ -34,7 +35,7 @@ onMounted(()=>getGoods())
           <div class="goods-info">
             <div class="media">
               <!-- 图片预览区 -->
-
+              <ImageView/>
               <!-- 统计数量 -->
               <ul class="goods-sales">
                 <li>
