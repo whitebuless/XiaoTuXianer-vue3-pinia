6d6944685668bcc2f029bc4f1c77394f8a726a8a
flash <2509719845@qq.com>
2023-06-03 13:16:49 +0800

    图片静态列表优化为props传参

--- src/components/index.vue
+++ src/components/index.vue
@@ -2,14 +2,23 @@
 import { ref,watch } from "vue"
 import { useMouseInElement } from "@vueuse/core"
 
+//props设配图片列表
+defineProps({
+  imageList:{
+    type:Array,
+    default:()=>[]
+  }
+})
+
+
 // 图片列表
-const imageList = [
-  "https://yanxuan-item.nosdn.127.net/d917c92e663c5ed0bb577c7ded73e4ec.png",
-  "https://yanxuan-item.nosdn.127.net/e801b9572f0b0c02a52952b01adab967.jpg",
-  "https://yanxuan-item.nosdn.127.net/b52c447ad472d51adbdde1a83f550ac2.jpg",
-  "https://yanxuan-item.nosdn.127.net/f93243224dc37674dfca5874fe089c60.jpg",
-  "https://yanxuan-item.nosdn.127.net/f881cfe7de9a576aaeea6ee0d1d24823.jpg"
-]
+// const imageList = [
+//   "https://yanxuan-item.nosdn.127.net/d917c92e663c5ed0bb577c7ded73e4ec.png",
+//   "https://yanxuan-item.nosdn.127.net/e801b9572f0b0c02a52952b01adab967.jpg",
+//   "https://yanxuan-item.nosdn.127.net/b52c447ad472d51adbdde1a83f550ac2.jpg",
+//   "https://yanxuan-item.nosdn.127.net/f93243224dc37674dfca5874fe089c60.jpg",
+//   "https://yanxuan-item.nosdn.127.net/f881cfe7de9a576aaeea6ee0d1d24823.jpg"
+// ]
 
 //小兔切换，大图显示
 const activeIndex=ref(0)
@@ -51,7 +60,7 @@ watch([elementX,elementY,isOutside],()=>{
 
 
 <template>
-  {{ elementX }},{{ elementY }},{{ isOutside }}
+  <!-- {{ elementX }},{{ elementY }},{{ isOutside }} -->
   <div class="goods-image">
     <!-- 左侧大图-->
     <div class="middle" ref="target">
