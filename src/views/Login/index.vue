51640ea751c31511cc8dd6ad35e49d4bac5e270e
flash <2509719845@qq.com>
2023-06-04 09:52:42 +0800

    自定义校验规则-勾选协议

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -4,7 +4,8 @@ import { ref} from 'vue'
 // 准备表单对象
 const form=ref({
   account:'',
-  password:''
+  password:'',
+  agree:true
 })
 // 准备规则对象
 const rules={
@@ -14,6 +15,17 @@ const rules={
   password:[
     {required:true,message:'密码不能为空',trigger:'blur'},
     {min:6,max:14,message:'密码长度介于6-14',trigger:'blur'}
+  ],
+  agree:[
+    {
+      validator:(rule,value,callback)=>{
+        if(value){
+          callback()
+        }else{
+          callback(new Error("请勾选协议"))
+        }
+      }
+    }
   ]
 }
 
@@ -49,8 +61,8 @@ const rules={
               <el-form-item prop="password" label="密码">
                 <el-input v-model="form.password" />
               </el-form-item>
-              <el-form-item label-width="22px">
-                <el-checkbox  size="large">
+              <el-form-item prop="agree" label-width="22px">
+                <el-checkbox v-model="form.agree" size="large">
                   我已同意隐私条款和服务条款
                 </el-checkbox>
               </el-form-item>
