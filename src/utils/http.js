00dbfe2fb275637ec7d4a8f1ce3ed3b629035d92
flash <2509719845@qq.com>
2023-06-04 12:18:12 +0800

    token失效的拦截处理

--- src/utils/http.js
+++ src/utils/http.js
@@ -2,6 +2,7 @@ import axios from "axios";
 import{ ElMessage } from 'element-plus'
 import 'element-plus/theme-chalk/el-message.css'
 import { useUserStore } from '@/stores/user'
+import  router  from '@/router'
 
 const httpInstance=axios.create({
   baseURL: 'http://pcapi-xiaotuxian-front-devtest.itheima.net',
@@ -23,11 +24,21 @@ httpInstance.interceptors.request.use(config => {
 
 // axios响应式拦截器
 httpInstance.interceptors.response.use(res => res.data, e => {
+  const userStore=useUserStore()
   // 统一错误提示
   ElMessage({
     type:'warning',
     message:e.response.data.message
   })
+  // 401token失效处理
+  // 清除本地用户数据
+
+  // 跳转到登录页
+  if(e.response.status===401){
+    userStore.clearUSerInfo()
+    router.push('/login')
+  }
+
   return Promise.reject(e)
 
 })
