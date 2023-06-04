b02cb8259389f845b47b775494a89998da2f7117
flash <2509719845@qq.com>
2023-06-04 09:59:18 +0800

    表单校验-同意校验

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -29,6 +29,22 @@ const rules={
   ]
 }
 
+// 获取form实例做统一校验
+const formRef=ref(null)
+
+const doLogin=()=>{
+  // 调用实例方法
+  formRef.value.validate ((valid)=>{
+    // valid所有项都通过才为true
+    if(valid){
+      console.log('login');
+    }
+    else{
+      console.log('nologin')
+    }
+  })
+}
+
 </script>
 
 
@@ -53,7 +69,7 @@ const rules={
         </nav>
         <div class="account-box">
           <div class="form">
-            <el-form :model="form" :rules="rules" label-position="right" label-width="60px"
+            <el-form ref="formRef" :model="form" :rules="rules" label-position="right" label-width="60px"
               status-icon>
               <el-form-item prop="account"  label="账户" >
                 <el-input v-model="form.account" />
@@ -66,7 +82,7 @@ const rules={
                   我已同意隐私条款和服务条款
                 </el-checkbox>
               </el-form-item>
-              <el-button size="large" class="subBtn">点击登录</el-button>
+              <el-button size="large" class="subBtn" @click="doLogin">点击登录</el-button>
             </el-form>
           </div>
         </div>
