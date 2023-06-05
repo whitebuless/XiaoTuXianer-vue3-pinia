fbea257debb972ea41ecef7117e29939770db4f5
flash <2509719845@qq.com>
2023-06-05 15:54:46 +0800

    地址切换

--- src/views/Checkout/index.vue
+++ src/views/Checkout/index.vue
@@ -16,6 +16,17 @@ onMounted(()=>getCheckInfo())
 // 控制弹窗打开
 const showDialog=ref(false)
 
+//切换地址
+const activeAddress=ref({})
+const switchAddress=(item)=>{
+  activeAddress.value=item
+}
+
+const confirm=()=>{
+  curAddress.value=activeAddress.value
+  showDialog.value=false
+  activeAddress.value={}
+}
 
 </script>
 
@@ -119,7 +130,7 @@ const showDialog=ref(false)
   <!-- 切换地址 -->
   <el-dialog v-model="showDialog" title="切换收货地址" width="30%" center>
   <div class="addressWrapper">
-    <div class="text item" v-for="item in checkInfo.userAddresses"  :key="item.id">
+    <div class="text item " :class="{active:activeAddress.id===item.id}" @click="switchAddress(item)" v-for="item in checkInfo.userAddresses"  :key="item.id">
       <ul>
       <li><span>收<i />货<i />人：</span>{{ item.receiver }} </li>
       <li><span>联系方式：</span>{{ item.contact }}</li>
@@ -130,7 +141,7 @@ const showDialog=ref(false)
   <template #footer>
     <span class="dialog-footer">
       <el-button>取消</el-button>
-      <el-button type="primary">确定</el-button>
+      <el-button type="primary" @click="confirm">确定</el-button>
     </span>
   </template>
   </el-dialog>
