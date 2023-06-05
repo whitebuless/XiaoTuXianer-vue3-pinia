48a046ef622034c50a3acfa312d2f9eb8a6bdcf9
flash <2509719845@qq.com>
2023-06-05 15:48:08 +0800

    地址切换打开弹框

--- src/views/Checkout/index.vue
+++ src/views/Checkout/index.vue
@@ -13,6 +13,8 @@ const getCheckInfo=async()=>{
 }
 onMounted(()=>getCheckInfo())
 
+// 控制弹窗打开
+const showDialog=ref(false)
 
 
 </script>
@@ -34,7 +36,7 @@ onMounted(()=>getCheckInfo())
               </ul>
             </div>
             <div class="action">
-              <el-button size="large" @click="toggleFlag = true">切换地址</el-button>
+              <el-button size="large" @click="showDialog = true" >切换地址</el-button>
               <el-button size="large" @click="addFlag = true">添加地址</el-button>
             </div>
           </div>
@@ -115,6 +117,23 @@ onMounted(()=>getCheckInfo())
     </div>
   </div>
   <!-- 切换地址 -->
+  <el-dialog v-model="showDialog" title="切换收货地址" width="30%" center>
+  <div class="addressWrapper">
+    <div class="text item" v-for="item in checkInfo.userAddresses"  :key="item.id">
+      <ul>
+      <li><span>收<i />货<i />人：</span>{{ item.receiver }} </li>
+      <li><span>联系方式：</span>{{ item.contact }}</li>
+      <li><span>收货地址：</span>{{ item.fullLocation + item.address }}</li>
+      </ul>
+    </div>
+  </div>
+  <template #footer>
+    <span class="dialog-footer">
+      <el-button>取消</el-button>
+      <el-button type="primary">确定</el-button>
+    </span>
+  </template>
+  </el-dialog>
   <!-- 添加地址 -->
 </template>
 
