f7426316c926bbb39bf284f15ac7afde4189b41d
flash <2509719845@qq.com>
2023-06-05 19:39:48 +0800

    获取基础的支付信息

--- src/views/Pay/index.vue
+++ src/views/Pay/index.vue
@@ -1,5 +1,14 @@
 <script setup>
-const payInfo = {}
+import { getOrderAPI } from '@/apis/pay'
+import { onMounted, ref } from 'vue'
+import { useRoute } from 'vue-router' 
+const route=useRoute()
+const payInfo = ref({})
+const getPayInfo=async ()=>{
+  const res=await getOrderAPI(route.query.id)
+  payInfo.value=res.result
+} 
+onMounted(()=>getPayInfo())
 </script>
 
 
